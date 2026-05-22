ace379d534066a62ae0d62cfcc02c44c8c1baee4
MILI-5 <sanjanawaghmare817@gmail.com>
2026-05-22 14:24:24 +0530

    Initial project setup

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("COVID-19 Forecasting Dashboard")
+
+st.write("Internship-level Time Series Forecasting Project")
---
+++ test_prophet.py
@@ -0,0 +1,5 @@
+# test_prophet.py
+
+from prophet import Prophet
+
+print("Prophet installed successfully!")
